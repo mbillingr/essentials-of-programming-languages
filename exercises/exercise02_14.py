4e18d7a014519536141cce3dcc62af33ff052ced
Martin Billinger <mbillingr@users.noreply.github.com>
2022-10-28 17:23:32 +0200

    exercise 2.14

---
+++ exercises/exercise02_14.py
@@ -0,0 +1,42 @@
+
+def empty_env():
+    return (
+        lambda search_var: report_no_binding_found(search_var),
+        lambda: True
+    )
+
+
+def extend_env(var, val, env):
+    return (
+        lambda search_var: val if var == search_var else apply_env(env, search_var),
+        lambda: False
+    )
+
+
+def apply_env(env, var):
+    return env[0](var)
+
+
+def is_empty_env(env):
+    return env[1]()
+
+
+# Instead of adding another procedure to the list and effectively reimplementing apply_env,
+# I'd rather catch the exception :)
+def has_binding(env, var):
+    try:
+        apply_env(env, var)
+    except LookupError:
+        return False
+    return True
+
+
+def report_no_binding_found(search_var):
+    raise LookupError(search_var)
+
+
+assert apply_env(extend_env("x", 42, empty_env()), "x") == 42
+assert is_empty_env(empty_env())
+assert not is_empty_env(extend_env("x", 42, empty_env()))
+assert has_binding(extend_env("x", 42, empty_env()), "x")
+assert not has_binding(empty_env(), "x")
